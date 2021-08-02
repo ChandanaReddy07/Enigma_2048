d398ab5839e851672367d56c75a46d28c28d0f1a
dcr7978933k <b219021@iiit-bh.ac.in>
2021-08-02 03:37:46 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,58 @@
-import React, { useState } from "react";
-import "./App.css";
-import { makeStyles } from "@material-ui/core/styles";
-import { CssBaseline } from "@material-ui/core";
-import Demo from "./components/demo";
-import PopUp from "./components/PopUp";
-import Contact from "./components/contact"
-import { createTheme } from "@material-ui/core/styles";
-import { ThemeProvider } from "@material-ui/styles";
+// import React, { useState } from "react";
+// import "./App.css";
+// import { makeStyles } from "@material-ui/core/styles";
+// import { CssBaseline } from "@material-ui/core";
+// import Demo from "./components/demo";
+// import PopUp from "./components/PopUp";
+// import Contact from "./components/contact"
+// import { createTheme } from "@material-ui/core/styles";
+// import { ThemeProvider } from "@material-ui/styles";
 
-import purple from "@material-ui/core/colors/purple";
-import green from "@material-ui/core/colors/green";
-import Start from "./components/start";
+// import purple from "@material-ui/core/colors/purple";
+// import green from "@material-ui/core/colors/green";
+// import Start from "./components/start";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minHeight: "100vh",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-  },
-}));
-const light = createTheme({
-  palette: {
-    type: "light",
-    primary: purple,
-    secondary: green,
-  },
-});
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     minHeight: "100vh",
+//     backgroundRepeat: "no-repeat",
+//     backgroundSize: "cover",
+//   },
+// }));
+// const light = createTheme({
+//   palette: {
+//     type: "light",
+//     primary: purple,
+//     secondary: green,
+//   },
+// });
 
-const dark = createTheme({
-  palette: {
-    type: "dark",
-    primary: purple,
-    secondary: green,
-  },
-});
+// const dark = createTheme({
+//   palette: {
+//     type: "dark",
+//     primary: purple,
+//     secondary: green,
+//   },
+// });
 
-function App() {
-  const classes = useStyles();
-  const [darkmode, setDarkmode] = useState(false);
-  const mode = createTheme({
-    palette: {
-      type: darkmode ? "dark" : "light",
-    },
-  });
+// function App() {
+//   const classes = useStyles();
+//   const [darkmode, setDarkmode] = useState(false);
+//   const mode = createTheme({
+//     palette: {
+//       type: darkmode ? "dark" : "light",
+//     },
+//   });
 
-  return (
-    <div className={classes.root}>
+//   return (
+//     <div className={classes.root}>
       
-      <ThemeProvider theme={mode}>
-        <CssBaseline />
-{/* 
-        <Demo darkmode={darkmode} setDarkmode={setDarkmode} />
-        {/* <PopUp/> */}
-        {/* <Contact/> */} 
-        
-      </ThemeProvider>
-    </div>
-  );
-}
+//       <ThemeProvider theme={mode}>
+//         <CssBaseline />
 
-export default App;
+//       </ThemeProvider>
+//     </div>
+//   );
+// }
+
+// export default App;
