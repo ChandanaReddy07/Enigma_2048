3ab54c9c381b7c826fdd35a27cec65e8ca67f3e0
dcr7978933k <b219021@iiit-bh.ac.in>
2021-08-02 03:58:45 -0700

    yo

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import "./details.css"
+import "./details.css";
 const Details = ({ image, data, i }) => {
   return (
     <div data-aos="fade-up">
-      <div
-        className="card"
-        
-      >
+      <div className="card">
         <div className="image" style={{ margin: "10px" }}>
-          <img
-            src={image}
-           
-            alt=""
-          />
+          <img src={image} alt="" />
         </div>
-        <div
-          className="desc"
-        
-        >
+        <div className="desc">
           <h1> Step {i + 1}</h1>
           {data}
         </div>
